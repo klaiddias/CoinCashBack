9b36745284b79d4b96fcc67dea143e9d66545792
klaiddias <klaiddias@gmail.com>
2022-03-01 00:38:10 +0000

    Finalizar Layout

--- CoinCashBack/Client/Program.cs
+++ CoinCashBack/Client/Program.cs
@@ -1,8 +1,11 @@
 using CoinCashBack.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
